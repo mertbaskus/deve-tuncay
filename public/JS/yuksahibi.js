e2559fc5f3bc54815bfd3968856e5bb4782c5579
mertbaskus <153538258+mertbaskus@users.noreply.github.com>
2024-01-06 20:02:14 +0300

    Delete public/JS/yuksahibi.js

--- public/JS/yuksahibi.js
+++
@@ -1,8 +0,0 @@
-
-const ilanVer = document.getElementById("ilanVer");
-const ilanVerButonu = document.getElementById("ilanVerButonu")
-
-ilanVerButonu.addEventListener("click", () => {
-    ilanVer.style.display = "block"
-})
-
